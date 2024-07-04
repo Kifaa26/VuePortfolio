cd2d328e66f8a0275a02270f9fa45ae3246b2974
Kifaa26 <akifahpalekar@gmail.com>
2024-07-05 01:11:51 +0200

    testimonials

--- src/components/TestimonialsSection.vue
+++ src/components/TestimonialsSection.vue
@@ -1,29 +1,65 @@
 <template>
-    <div class="container">
-      <h2>Testimonials</h2>
-      <div v-for="(testimonial, index) in testimonials" :key="index" class="testimonial">
-        <img :src="testimonial.image" alt="Testimonial Author" class="testimonial-image">
-        <blockquote class="testimonial-quote">{{ testimonial.quote }}</blockquote>
-        <p class="testimonial-author">{{ testimonial.name }}</p>
+  <div class="container">
+    <h2>Testimonials</h2>
+    <div class="row">
+      <div class="col-md-6 col-lg-4" v-for="(testimonial, index) in testimonials" :key="index">
+        <Card class="testimonial-card">
+          <template v-slot:image>
+            <img :src="testimonial.image" alt="Testimonial Author" class="card-img-top">
+          </template>
+          <template v-slot:quote>
+            <blockquote class="card-text">{{ testimonial.quote }}</blockquote>
+          </template>
+          <template v-slot:author>
+            <h4 class="card-author">{{ testimonial.name }}</h4>
+          </template>
+        </Card>
       </div>
     </div>
-  </template>
-  
-  <script>
+  </div>
+</template>
 
-  export default {
-    name: "TestimonialsSection",
-    computed: {
-        testimonials() {
-            return this.$store.state.testimonials
-        }
-    },
-    mounted() {
-        this.$store.dispatch('getTestimonials')
+<script>
+import Card from './CardComp.vue'; 
+
+export default {
+  name: "TestimonialsSection",
+  components: {
+    Card
+  },
+  computed: {
+    testimonials() {
+      return this.$store.state.testimonials;
     }
-  };
-  </script>
+  },
+  mounted() {
+    this.$store.dispatch('getTestimonials');
+  }
+};
+</script>
 
-  <style scoped>
-  
-  </style>
+<style scoped>
+.container {
+  margin-top: 20px;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.card-text {
+  color: #ffff;
+  text-shadow: 2px 2px 5px #000000;
+}
+
+.card-author {
+  color: #ffff;
+  text-shadow: 2px 2px 5px #000000;
+}
+
+.testimonial-card {
+  height: 95%; 
+}
+</style>
